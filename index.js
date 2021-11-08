e8d56dc65904891b1609e23c14c4976f335c2b5e
rakibshakib <rakibshakib74@gmail.com>
2021-11-08 23:57:08 +0600

    set env and update server jwt file

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const app = express();
+const admin = require("firebase-admin");
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
+
+// firebase token varificatiion 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+// require("./doctors-portal-rs-firebase-adminsdk.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
 // middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSER}/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        } catch {
+
+        }
+    }
+    next();
+}
 
 async function run() {
     try {
@@ -19,7 +42,6 @@ async function run() {
         const appoimentData = DoctorsPortal.collection("appoimentData");
         const userData = DoctorsPortal.collection("user_data");
         // create a document to insert
-        console.log("connented to mongoDB");
 
         // post appoimentData to database 
         app.post("/appoinment-data", async (req, res) => {
@@ -28,7 +50,7 @@ async function run() {
             res.json(result)
         })
         // get appoinments data 
-        app.get("/appoinmets-all-data", async (req, res) => {
+        app.get("/appoinmets-all-data", verifyToken, async (req, res) => {
             const email = req.query.email;
             const date = new Date(req.query.date).toLocaleDateString();
             const query = { email: email, date: date }
@@ -47,12 +69,41 @@ async function run() {
             const user = req.body;
             const filterUser = { email: user.email };
             const options = { upsert: true };
-            const updateUser = {
-                $set: user
-            }
+            const updateUser = { $set: user }
             const result = await userData.updateOne(filterUser, updateUser, options)
             res.json(result)
         })
+
+        // set admin 
+        app.put("/users-data/admin", verifyToken, async (req, res) => {
+            const user = req.body;
+            // const token = req.headers.authorization;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await userData.findOne({ email: requester })
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } }
+                    const result = await userData.updateOne(filter, updateDoc);
+                    res.json(result)
+                }
+            } else {
+                res.status(403).json({ message: "You dont have permission to make this user admin" })
+            }
+        })
+        // get user data as a admin
+        app.get('/users-data/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userData.findOne(query);
+            let isAdmin = false
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin })
+        })
+
+
     } finally {
         //   await client.close();
     }
